a3ea3f1ab0cba56034c04fefc1b88d4396c85499
RosalindFok <huoyufu19@mails.ucas.ac.cn>
2024-01-30 17:27:47 +0800

    meta_sam_checkpoints

---
+++ meta_sam_checkpoints/run_download_checkpoints.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://github.com/facebookresearch/segment-anything#model-checkpoints
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
